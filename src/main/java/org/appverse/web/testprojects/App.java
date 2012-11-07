feff8407c5a5a36ee7bda846c3f9b46cdeacd853
carlosrubio <carlos.rubio@gft.com>
2012-11-07 22:39:49 +0100

    Import git-release-test

---
+++ src/main/java/org/appverse/web/testprojects/App.java
@@ -0,0 +1,13 @@
+package org.appverse.web.testprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
